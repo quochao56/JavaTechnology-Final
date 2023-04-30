49abea037d88f9f81190ff5c11568a92f6893726
ngovi <74971162+ohwonwi@users.noreply.github.com>
2023-04-30 14:11:55 +0700

    Delete CategoryRepository.java

--- Website/src/main/java/tdtu/edu/vn/finalproject/Repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package tdtu.edu.vn.finalproject.Repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tdtu.edu.vn.finalproject.Model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
